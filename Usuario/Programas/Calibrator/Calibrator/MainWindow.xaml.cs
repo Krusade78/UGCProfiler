de5af60cbfddaad3b65980f14639acc0f8e1f93a
Alfredo <krusade325@hotmail.com>
2017-07-19 15:38:11 +0000

    git-tfs-id: [https://alfredocos.visualstudio.com]$/Driver joystick;C83

--- Usuario/Programas/Calibrator/Calibrator/MainWindow.xaml.cs
+++ Usuario/Programas/Calibrator/Calibrator/MainWindow.xaml.cs
@@ -10,9 +10,6 @@ namespace Calibrator
     /// </summary>
     public partial class MainWindow : Window
     {
-        private byte tipo = 255;
-        private System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer(System.Windows.Threading.DispatcherPriority.Background);
-
         public MainWindow()
         {
             InitializeComponent();
@@ -20,41 +17,39 @@ namespace Calibrator
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            //uint n = 0;
+            uint n = 0;
             IntPtr hJoy = IntPtr.Zero;
-            //CRawInput.GetRawInputDeviceList(null, ref n, (uint)Marshal.SizeOf(typeof(CRawInput.RAWINPUTDEVICELIST)));
-            //if (n > 0)
-            //{
-            //    CRawInput.RAWINPUTDEVICELIST[] lid = new CRawInput.RAWINPUTDEVICELIST[n];
-            //    CRawInput.GetRawInputDeviceList(lid, ref n, (uint)Marshal.SizeOf(typeof(CRawInput.RAWINPUTDEVICELIST)));
-            //    foreach (CRawInput.RAWINPUTDEVICELIST l in lid)
-            //    {
-            //        IntPtr buff = Marshal.AllocHGlobal(256);
-            //        uint cbsize = 128;
-            //        uint ret = CRawInput.GetRawInputDeviceInfo(l.hDevice, 0x20000007, buff, ref cbsize);
-            //        String nombre = Marshal.PtrToStringAuto(buff);
-            //        Marshal.FreeHGlobal(buff);
-            //        if (nombre.StartsWith("\\\\?\\HID#VID_06A3&PID_0255#"))
-            //        {
-            //            hJoy = l.hDevice;
-            //            break;
-            //        }
-            //cbsize = (uint)Marshal.SizeOf(typeof(CRawInput.DeviceInfo));
-            //buff = Marshal.AllocHGlobal((int)cbsize);
-            //CRawInput.GetRawInputDeviceInfo(l.hDevice, 0x2000000b, buff, ref cbsize);
-            //CRawInput.DeviceInfo di = Marshal.PtrToStructure<CRawInput.DeviceInfo>(buff);
-            //Marshal.FreeHGlobal(buff);
-            //    }
-            //}
+            CRawInput.GetRawInputDeviceList(IntPtr.Zero, ref n, (uint)Marshal.SizeOf(typeof(CRawInput.RawInputDeviceList)));
+            if (n > 0)
+            {
+                IntPtr lid = Marshal.AllocHGlobal((int)((uint)Marshal.SizeOf(typeof(CRawInput.RawInputDeviceList)) * n));
+                if ((int)CRawInput.GetRawInputDeviceList(lid, ref n, (uint)Marshal.SizeOf(typeof(CRawInput.RawInputDeviceList))) != -1)
+                {
+                    for (uint i = 0; i < n; i++)
+                    {
+                        CRawInput.RawInputDeviceList l = (CRawInput.RawInputDeviceList)Marshal.PtrToStructure(new IntPtr((lid.ToInt32() + ((uint)Marshal.SizeOf(typeof(CRawInput.RawInputDeviceList)) * i))), typeof(CRawInput.RawInputDeviceList));
+                        IntPtr buff = Marshal.AllocHGlobal(256);
+                        uint cbsize = 128;
+                        uint ret = CRawInput.GetRawInputDeviceInfo(l.DeviceHandle, CRawInput.RawInputDeviceInfoCommand.DeviceName, buff, ref cbsize);
+                        String nombre = Marshal.PtrToStringAuto(buff);
+                        Marshal.FreeHGlobal(buff);
+                        if (nombre.StartsWith("\\\\?\\HID#VID_06A3&PID_0255#"))
+                        {
+                            hJoy = l.DeviceHandle;
+                            break;
+                        }
+                        //cbsize = (uint)Marshal.SizeOf(typeof(CRawInput.DeviceInfo));
+                        //buff = Marshal.AllocHGlobal((int)cbsize);
+                        //CRawInput.GetRawInputDeviceInfo(l.DeviceHandle, 0x2000000b, buff, ref cbsize);
+                        //CRawInput.DeviceInfo di = Marshal.PtrToStructure<CRawInput.DeviceInfo>(buff);
+                        //Marshal.FreeHGlobal(buff);
+                    }
+                }
+                Marshal.FreeHGlobal(lid);
+            }
 
-            CRawInput.RAWINPUTDEVICE[] rid = new CRawInput.RAWINPUTDEVICE[1];
-
-            rid[0].UsagePage = 0x01;
-            rid[0].Usage = 0x04;
-            rid[0].Flags = 0;
-            rid[0].WindowHandle = new System.Windows.Interop.WindowInteropHelper(this).Handle;
-            CRawInput.RegisterRawInputDevices(rid, 1, Marshal.SizeOf(rid[0]));
-            ucInfo.Iniciar(hJoy);           
+            ucInfo.Iniciar(hJoy);
+            ucCalibrar.Iniciar(hJoy);
         }
 
         private void toggleButton_Checked(object sender, RoutedEventArgs e)
