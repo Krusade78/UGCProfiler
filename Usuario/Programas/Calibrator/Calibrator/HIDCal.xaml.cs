e5ba7dbb72b617d1679746f9b192af128b2f6aae
Alfredo <krusade325@hotmail.com>
2017-07-28 11:47:51 +0000

    Updated 5 files in Calibrator
    git-tfs-id: [https://alfredocos.visualstudio.com]$/Driver joystick;C98

--- Usuario/Programas/Calibrator/Calibrator/HIDCal.xaml.cs
+++ Usuario/Programas/Calibrator/Calibrator/HIDCal.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
 
 namespace Calibrator
 {
@@ -20,9 +21,188 @@ namespace Calibrator
     /// </summary>
     public partial class HIDCal : UserControl
     {
+        private IntPtr hJoy;
+        private System.Windows.Threading.DispatcherTimer timer;
+        private int ejeSel = 0;
+        private struct STLIMITES
+        {
+            internal bool cal;
+            internal ushort i;
+            internal ushort c;
+            internal ushort d;
+            internal ushort n;
+        };
+        private struct STJITTER
+        {
+            internal bool antiv;
+            internal long PosElegida;
+            internal byte PosRepetida;
+            internal byte Margen;
+            internal byte Resistencia;
+        };
+
+        STJITTER jitter = new STJITTER();
+        STLIMITES limites = new STLIMITES();
+
         public HIDCal()
         {
             InitializeComponent();
+            timer = new System.Windows.Threading.DispatcherTimer(System.Windows.Threading.DispatcherPriority.Input);
+            timer.Interval = new System.TimeSpan(50);
+            timer.Tick += Timer_Tick;
+
+            limites.cal = true;
+            jitter.antiv = true;
+        }
+
+        public void Iniciar(IntPtr hJoy)
+        {
+            this.hJoy = hJoy;
+        }
+
+        private void Timer_Tick(object sender, System.EventArgs e)
+        {
+            if (hJoy == IntPtr.Zero)
+                return;
+
+            int cbsize = 0;
+            int ret = CRawInput.GetRawInputData(hJoy, CRawInput.RawInputCommand.Input, IntPtr.Zero, ref cbsize, Marshal.SizeOf(typeof(CRawInput.RAWINPUTHEADER)));
+            int err = Marshal.GetLastWin32Error();
+            if (ret == 0 && cbsize != 0)
+            {
+                cbsize *= 8;
+                IntPtr buff = Marshal.AllocHGlobal(cbsize);
+                ret = CRawInput.GetRawInputData(hJoy, CRawInput.RawInputCommand.Input, buff, ref cbsize, Marshal.SizeOf(typeof(CRawInput.RAWINPUTHEADER)));
+                if (ret > 0)
+                {
+                    CRawInput.RawInput ri = Marshal.PtrToStructure<CRawInput.RawInput>(buff);
+                    int leidos = 0;
+                    while (true)
+                    {
+                        if (ri.Header.Type == CRawInput.RawInputType.HID)
+                        {
+                            byte[] data = new byte[ri.HID.Count * ri.HID.Size];
+                            Marshal.Copy((IntPtr)(buff.ToInt64() + Marshal.SizeOf(typeof(CRawInput.RAWINPUTHEADER)) + Marshal.SizeOf(typeof(CRawInput.RawHID))), data, 0, data.Length);
+                            byte[] hidData = new byte[data.Length];
+                            ActualizarEstado(hidData);
+                        }
+                        leidos += ri.Header.Size + 1;
+                        ret--;
+                        if (ret > 0)
+                            ri = Marshal.PtrToStructure<CRawInput.RawInput>(buff + leidos);
+                        else
+                            break;
+                    }
+                }
+                Marshal.FreeHGlobal(buff);
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void ActualizarEstado(byte[] hidData)
+        {
+            int posr = (hidData[ejeSel * 1] << 8) | hidData[(ejeSel * 2) + 1];
+            txtPosReal.Text = posr.ToString();
+            posReal.Margin = new Thickness(posr - 5, 0, 0, 0);
+
+            // Filtrado de ejes
+            long pollEje = (ushort)posr;
+
+            if (jitter.antiv)
+            {
+                // Antivibraciones
+                if ((pollEje < (jitter.PosElegida - jitter.Margen)) || (pollEje > (jitter.PosElegida + jitter.Margen)))
+                {
+                    jitter.PosRepetida = 0;
+                    jitter.PosElegida = pollEje;
+                }
+                else
+                {
+                    if (jitter.PosRepetida < jitter.Resistencia)
+                    {
+                        jitter.PosRepetida++;
+                        pollEje = jitter.PosElegida;
+                    }
+                    else
+                    {
+                        jitter.PosRepetida = 0;
+                        jitter.PosElegida = pollEje;
+                    }
+                }
+            }
+
+            if (limites.cal)
+            {
+                // Calibrado
+                ushort ancho1, ancho2;
+                ancho1 = (ushort)((limites.c - limites.n) - limites.i);
+                ancho2 = (ushort)(limites.d - (limites.c + limites.n));
+                if (((pollEje >= (limites.c - limites.n)) && (pollEje <= (limites.c + limites.n))))
+                {
+                    //Zona nula
+                    pollEje = 1024;
+                }
+                else
+                {
+                    if (pollEje < limites.i)
+                        pollEje = limites.i;
+                    if (pollEje > limites.d)
+                        pollEje = limites.d;
+
+                    if (pollEje < limites.c)
+                    {
+                        pollEje = ((limites.c - limites.n) - pollEje);
+                        if (ancho1 > ancho2)
+                        {
+                            pollEje = (pollEje * ancho2) / ancho1;
+                        }
+                        pollEje = 0 - pollEje;
+                    }
+                    else
+                    {
+                        pollEje -= (limites.c + limites.n);
+                        if (ancho2 > ancho1)
+                            pollEje = ((pollEje * ancho1) / ancho2);
+                    }
+
+                    if (ancho2 > ancho1)
+                        pollEje = ((pollEje + ancho1) * (2048)) / (2 * ancho1);
+                    else
+                        pollEje = ((pollEje + ancho2) * (2048)) / (2 * ancho2);
+                }
+            }
+
+            txtPosCal.Text = pollEje.ToString();
+            posCal.Margin = new Thickness(pollEje - 5, 0, 0, 0);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ushort s = 0;
+            ushort.TryParse(txtI.Text, out s);
+            limites.i = s;
+            ushort.TryParse(txtC.Text, out s);
+            limites.c = s;
+            ushort.TryParse(txtD.Text, out s);
+            limites.d = s;
+            ushort.TryParse(txtN.Text, out s);
+            limites.n = s;
+
+            ushort.TryParse(txtMargen.Text, out s);
+            if (s > 255) s = 255;
+            jitter.Margen = (byte)s;
+            ushort.TryParse(txtResistencia.Text, out s);
+            if (s > 255) s = 255;
+            jitter.Resistencia = (byte)s;
         }
     }
 }
--- Usuario/Programas/Calibrator/Calibrator/Info.xaml.cs
+++ Usuario/Programas/Calibrator/Calibrator/Info.xaml.cs
@@ -10,7 +10,6 @@ namespace Calibrator
     /// </summary>
     public partial class Info : UserControl
     {
-
         IntPtr hJoy;
         System.Windows.Threading.DispatcherTimer timer;
 
@@ -113,28 +112,34 @@ namespace Calibrator
             Labelxy.Text = "mX: " + mx + "\n" + "mY: " + my;
             ejeMXY.Margin = new Thickness(mx, my, 0, 0);
 
-            
+            System.Windows.Shapes.Ellipse[] bts = new System.Windows.Shapes.Ellipse[] { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8,
+                                                                                        bt9, bt10, bt11, bt12, bt13, bt14, bt15, bt16,
+                                                                                        bt17, bt18, bt19, bt20, bt21, bt22, bt23, bt24,
+                                                                                        bt25, bt26, bt27, bt28, bt29, bt30, bt31, bt32};
             for(int i = 0; i < 32; i++)
-            {
+                bts[i].Visibility = ((hidData[20 + (i / 8)] & (1 << (i % 8))) != 0) ? Visibility.Visible : Visibility.Hidden;
 
-            }
-
-            textBlock.Text = "";
-            for (int i = 0; i < hidData.Length; i++)
-                textBlock.Text += hidData[i].ToString() + ",";
+            pathP1.Visibility = (hidData[16] == 0) ? Visibility.Hidden : Visibility.Visible;
+            pathP1.RenderTransform.Value.Rotate((hidData[16] > 5) ? (hidData[16] - 9) * 45 : (hidData[16] - 1) * 45);
+            pathP2.Visibility = (hidData[17] == 0) ? Visibility.Hidden : Visibility.Visible;
+            pathP2.RenderTransform.Value.Rotate((hidData[16] > 5) ? (hidData[16] - 9) * 45 : (hidData[16] - 1) * 45);
+            pathP3.Visibility = (hidData[18] == 0) ? Visibility.Hidden : Visibility.Visible;
+            pathP3.RenderTransform.Value.Rotate((hidData[16] > 5) ? (hidData[16] - 9) * 45 : (hidData[16] - 1) * 45);
+            pathP4.Visibility = (hidData[19] == 0) ? Visibility.Hidden : Visibility.Visible;
+            pathP4.RenderTransform.Value.Rotate((hidData[16] > 5) ? (hidData[16] - 9) * 45 : (hidData[16] - 1) * 45);
         }
 
         #region "Saltar perfil"
-        [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true, CharSet = System.Runtime.InteropServices.CharSet.Unicode)]
-        private static extern Microsoft.Win32.SafeHandles.SafeFileHandle CreateFile(string lpFileName, System.UInt32 dwDesiredAccess, System.UInt32 dwShareMode, System.IntPtr pSecurityAttributes, System.UInt32 dwCreationDisposition, System.UInt32 dwFlagsAndAttributes, System.IntPtr hTemplateFile);
-        [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        private static extern Microsoft.Win32.SafeHandles.SafeFileHandle CreateFile(string lpFileName, UInt32 dwDesiredAccess, UInt32 dwShareMode, IntPtr pSecurityAttributes, UInt32 dwCreationDisposition, UInt32 dwFlagsAndAttributes, IntPtr hTemplateFile);
+        [DllImport("kernel32.dll", SetLastError = true)]
         private static extern bool DeviceIoControl(Microsoft.Win32.SafeHandles.SafeFileHandle handle, UInt32 dwIoControlCode, byte[] lpInBuffer, UInt32 nInBufferSize, byte[] lpOutBuffer, UInt32 nOutBufferSize, out UInt32 lpBytesReturned, IntPtr lpOverlapped);
         private void checkBox_Checked(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.SafeHandles.SafeFileHandle driver = CreateFile(
                     "\\\\.\\XUSBInterface",
-                    0x40000000,//GENERIC_WRITE,
-                    0x00000002, //FILE_SHARE_WRITE,
+                    0x80000000 | 0x40000000,//GENERIC_WRITE | GENERIC_READ,
+                    0x00000002 | 0x00000001, //FILE_SHARE_WRITE | FILE_SHARE_READ,
                     IntPtr.Zero,
                     3,//OPEN_EXISTING,
                     0,
@@ -143,7 +148,7 @@ namespace Calibrator
                 return;
 
             UInt32 ret = 0;
-            UInt32 IOCTL_USR_RAW = ((0x22) << 16) | ((2) << 14) | ((0x0108) << 2) | (0);
+            UInt32 IOCTL_USR_RAW = ((0x22) << 16) | ((2) << 14) | ((0x0808) << 2) | (0);
             byte[] buff = new byte[] { 1 };
             if (!DeviceIoControl(driver, IOCTL_USR_RAW, buff, 1, null, 0, out ret, IntPtr.Zero))
             {
@@ -158,8 +163,8 @@ namespace Calibrator
         {
             Microsoft.Win32.SafeHandles.SafeFileHandle driver = CreateFile(
                     "\\\\.\\XUSBInterface",
-                    0x40000000,//GENERIC_WRITE,
-                    0x00000002, //FILE_SHARE_WRITE,
+                    0x80000000 | 0x40000000,//GENERIC_WRITE | GENERIC_READ,
+                    0x00000002 | 0x00000001, //FILE_SHARE_WRITE | FILE_SHARE_READ,
                     IntPtr.Zero,
                     3,//OPEN_EXISTING,
                     0,
@@ -168,7 +173,7 @@ namespace Calibrator
                 return;
 
             UInt32 ret = 0;
-            UInt32 IOCTL_USR_RAW = ((0x22) << 16) | ((2) << 14) | ((0x0108) << 2) | (0);
+            UInt32 IOCTL_USR_RAW = ((0x22) << 16) | ((2) << 14) | ((0x0808) << 2) | (0);
             byte[] buff = new byte[] { 0 };
             if (!DeviceIoControl(driver, IOCTL_USR_RAW, buff, 1, null, 0, out ret, System.IntPtr.Zero))
             {
