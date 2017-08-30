62d99cb2d39706f7192e578e694770b6600e3cee
Alfredo <krusade325@hotmail.com>
2017-08-30 12:06:38 +0000

    Added 2 files to Launcher
    git-tfs-id: [https://alfredocos.visualstudio.com]$/Driver joystick;C138

---
+++ Usuario/Programas/Launcher/MenuLauncher.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NotificationIconSample
+{
+    /// <summary>
+    /// Lógica de interacción para UserControl1.xaml
+    /// </summary>
+    public partial class MenuLauncher : Window
+    {
+
+        public MenuLauncher()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Activate();
+            this.ContextMenu.IsOpen = true;
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
